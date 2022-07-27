2f89f849cfcb90da439ed44eafa1c0f353e5c86b
Serj <93035922+Serj-Kyleikin@users.noreply.github.com>
2022-07-27 23:27:01 +0300

    Update MainModel.php

--- application/models/MainModel.php
+++ application/models/MainModel.php
@@ -28,30 +28,38 @@ class MainModel extends Model {
         } catch(\PDOException $e) {
             logError($e, 1);
         }
-
-        // Поиск первой статьи из БД в выдаче
-
-        try {
-
-            $getId = $this->connection->prepare('SELECT id FROM articles ORDER BY id LIMIT 1');
-            $getId->execute();
-            $min = $getId->fetch(PDO::FETCH_ASSOC);
-
-        } catch(\PDOException $e) {
-            logError($e, 1);
-        }
-
-        $options = '';                          // id постов дял динамического контента
-
-        foreach($result['static'] as $article) {
-            $options .= $article['id'] . ',';
-            if($article['id'] == $min['id']) $result['pagination']['next'] = false;
-        }
         
-        $options = trim($options, ',');
+        if($result['static']) {
 
-        $result['dynamic']['options'] = $options;
-        $result['dynamic']['content'] = $this->getMainDynamic($options);
+            // Поиск первой статьи из БД в выдаче
+
+            try {
+
+                $getId = $this->connection->prepare('SELECT id FROM articles ORDER BY id LIMIT 1');
+                $getId->execute();
+                $min = $getId->fetch(PDO::FETCH_ASSOC);
+
+            } catch(\PDOException $e) {
+                logError($e, 1);
+            }
+
+            $options = '';                          // id постов дял динамического контента
+
+            foreach($result['static'] as $article) {
+                $options .= $article['id'] . ',';
+                if($article['id'] == $min['id']) $result['pagination']['next'] = false;
+            }
+
+            $options = trim($options, ',');
+
+            $result['dynamic']['options'] = $options;
+            $result['dynamic']['content'] = $this->getMainDynamic($options);
+
+        } else {
+
+            if(!$from) $result['pagination']['next'] = false;       // Нет записей для первой страницы в пагинации
+            else $result['static']['empty'] = true;                 // Отсутствуют данные для пагинации
+        }
 
         return $result;
     }
